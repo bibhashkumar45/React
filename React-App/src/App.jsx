2dbee91c4ff05b6b89ed4f35a2f2dbef702a38f8
Bibhash Kumar <bibhashkumar4587@gmail.com>
2024-08-09 03:06:55 +0530

    React component

---
+++ React-App/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './Hello'
+import Random from './Randon'
+
+function App() {
+
+
+  return <div>
+  <h1>Bibhash Kumar</h1>
+  <Hello></Hello>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+  <Random></Random>
+
+  </div>
+
+  
+}
+
+export default App
---
+++ React-App/src/Hello.jsx
@@ -0,0 +1,8 @@
+function Hello(){
+  
+  return <div>
+    <h1>This is my first program in React</h1>
+  </div>
+
+}
+export default Hello;
