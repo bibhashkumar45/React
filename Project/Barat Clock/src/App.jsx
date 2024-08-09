b497b6f96b6fd2a29c7842eed7a143aca6ad33ac
Bibhash Kumar <bibhashkumar4587@gmail.com>
2024-08-09 10:54:51 +0530

    Bharat Clock

---
+++ Project/Barat Clock/src/App.jsx
@@ -0,0 +1,16 @@
+import Title from "./components/Title"
+import Description from "./components/Description"
+import Timer from "./components/Timer"
+import "./App.css"
+function App() {
+ 
+
+  return <center className="container">
+    <Title/>
+    <Description/>
+    <Timer/>
+  </center>
+  
+}
+
+export default App
---
+++ Project/Barat Clock/src/components/Title.jsx
@@ -0,0 +1,6 @@
+function Title()
+{
+  return <h1>Bharat Clock</h1>
+}
+
+export default Title;
