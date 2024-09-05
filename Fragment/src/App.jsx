387beeac3180832bac97739943c0a71249a693dd
Bibhash Kumar <bibhashkumar4587@gmail.com>
2024-09-06 01:22:36 +0530

    Fragement in React

--- Fragment/src/App.jsx
+++ Fragment/src/App.jsx
@@ -5,26 +5,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-  // let FoodItems=["Dal","Hari Sabji","Anar","Bajra","Makka di roti"];
-  let FoodItems=[];
-  // let emptyMassage=FoodItems.length==0? <h1>I am still hungry</h1>:null;
 
+//   return <React.Fragment>
+//   <h1>Food Items</h1>
+//   <ul class="list-group">
+//   <li class="list-group-item">Dal</li>
+//   <li class="list-group-item">Green Vagitable</li>
+//   <li class="list-group-item">Roti</li>
+//   <li class="list-group-item">Paneer</li>
+//   <li class="list-group-item">Samosa</li>
+// </ul>
+//   </React.Fragment>
 
-  return  <React.Fragment>
-  <h1>Healthy Food</h1>
-
-  {/* This conditial statement (Ternary statement) use instant of if-else statement */}
-  {/* {emptyMassage}; */}
-
-{/* Logical Operator */}
-  {FoodItems.length== 0 && <h1>I am still hungry</h1>}    
-  <ul className="list-group">
-  {FoodItems.map((item)=> <li key={item}  className="list-group-item">{item}</li>)};  
-  {/* Key use to assign special number to each and every list that are  present on Frontend */}
-  {/* map os the alternate  of loop in jsx */}
-
+return <>
+<h1>Food Items</h1>
+<ul class="list-group">
+<li class="list-group-item">Dal</li>
+<li class="list-group-item">Green Vagitable</li>
+<li class="list-group-item">Roti</li>
+<li class="list-group-item">Paneer</li>
+<li class="list-group-item">Samosa</li>
 </ul>
-</React.Fragment>
+</>
 }
 
 export default App
