f2c9f2574289ff78a15f848e664a83adf84936c2
Bibhash Kumar <bibhashkumar4587@gmail.com>
2024-09-06 02:08:30 +0530

    Map Method and conditional rendering

--- MapMethod/MapMethod/src/App.jsx
+++ MapMethod/MapMethod/src/App.jsx
@@ -2,12 +2,26 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//      --------------------------------------- Conditional Rending -----------------------------
 function App() {
+
   let FoodItems=["Dal","Green Vagitable","Roti","Paneer","Samosa"];
+  // let FoodItems=[];
+
+  
+
+  // --------------- It will apply on whole page -------------------
+  // if(FoodItems.length==0){
+  //   return <li>I am still hungry</li>  
+  // }
 
   return (
     <>
       <h1>Food Items</h1>
+      {/* Ternary Statement */}
+      {/* {FoodItems.length==0?<li>I am still hungry</li>:null} */}
+      {/* Logical Statement */}
+      {FoodItems.length==0&& <li>I am still hungry</li>}
 <ul className="list-group">
   {FoodItems.map(item=><li key={item} className="list-group-item">{item}</li>)}
 </ul>
@@ -19,3 +33,18 @@ export default App
 
 
 // Key is assign to individual element so that if there  is one update in one element then it will not update entire page
+
+
+
+// --------------------------- Map Meyhod ----------------------------------------------------------------
+// {
+//   let FoodItems=["Dal","Green Vagitable","Roti","Paneer","Samosa"];
+//   return (
+//     <>
+//       <h1>Food Items</h1>
+// <ul className="list-group">
+//   {FoodItems.map(item=><li key={item} className="list-group-item">{item}</li>)}
+// </ul>
+//     </>
+//   )
+// }
