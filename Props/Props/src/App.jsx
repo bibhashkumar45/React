446300659890c86dd7f73508084940623836704a
Bibhash Kumar <bibhashkumar4587@gmail.com>
2024-09-06 21:33:02 +0530

    Probs in React

--- Props/Props/src/App.jsx
+++ Props/Props/src/App.jsx
@@ -1,15 +1,16 @@
 
-import './App.css'
+
 import FoodItem from './component/FoodItem';
 import ErrorMassage from './component/ErrorMassage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   let FoodItems=["Bhat","Green Vagitable","Roti","Paneer","Samosa"];
   // let FoodItems=[];
   return (
     <>
-    <h1>Food Menu</h1>
+    <h1 className="Header">Food Menu</h1>
     <ErrorMassage errorAttributes={FoodItems}></ErrorMassage>
     <FoodItem foodListAttributes={FoodItems}/>
     </>
--- Props/Props/src/component/Items.jsx
+++ Props/Props/src/component/Items.jsx
@@ -1,10 +1,16 @@
+import styles from "./Items.module.css";
 let Items=({foody})=>
 {
   // let {foody}=props;
   return (
     <>
-    <li className="list-group-item">{foody}</li>
+    {/* Regulat css */}
+    {/* <li className="list-group-item bg-food"><span className="Food-weight">{foody}</span></li> */}
+    <li className={`${styles["bg-food"]} `}><span className={styles["Food-weight"]}>{foody}</span></li>
     </>
   );
 }
 export default Items;
+
+
+// list-group-item bg-food
